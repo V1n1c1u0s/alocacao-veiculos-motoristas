2e345767d0c9954c9aaaf0f1f0c173d32259a3e2
V1n1c1u0s <caiovinicius2700@gmail.com>
2023-04-09 07:57:45 -0300

    organizando-estrutura

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Pages/Home.module.css'
 import { useState } from 'react'
 import { setCookie } from 'cookies-next'
 import { useRouter } from 'next/router'
