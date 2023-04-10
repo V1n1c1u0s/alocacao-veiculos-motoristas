9c206b5ea399a968e2845ae5e5fbc105336ea3a6
V1n1c1u0s <caiovinicius2700@gmail.com>
2023-04-09 22:09:42 -0300

    form-inicial

--- src/componentes/Forms/Agendamento.js
+++ src/componentes/Forms/Agendamento.js
@@ -4,10 +4,17 @@ export default function Agendamento(){
   return(
     <>
       <form className={styles.form}>
-        <input type='text' placeholder='Hora'></input>
-        <input type='text' placeholder='Local'></input>
-        <input type='datetime-local' placeholder='Data'></input>
+        <h1>Sistema Para Locação de Veículo da Universidade</h1>
+        <input type='text' placeholder='Data' min="2023-04-05" max="2023-12-31"></input>
+        <input type='text' placeholder='Horário de Ida'></input>
+        <input type='text' placeholder='Horário de Volta'></input>
+        <input type='text' placeholder='CEP'></input>
+        <input type='text' placeholder='Endereço'></input>
+        <input type='text' placeholder='Bairro'></input>
+        <input type='text' placeholder='Cidade'></input>
+        <input type='number' placeholder='N° Do Estabelecimento'></input>
         <input type='number' placeholder='N° de Pessoas'></input>
+        <button type='submit'>Enviar</button>
       </form>
     </>
   )
